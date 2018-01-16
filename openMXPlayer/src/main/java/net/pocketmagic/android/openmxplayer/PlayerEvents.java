2c1466db73d131a02c6743c285125adf7e05dcb0
realone <realone85@gmail.com>
2018-01-16 18:34:21 +0900

    formatting

--- openMXPlayer/src/main/java/net/pocketmagic/android/openmxplayer/PlayerEvents.java
+++ openMXPlayer/src/main/java/net/pocketmagic/android/openmxplayer/PlayerEvents.java
@@ -2,10 +2,10 @@ package net.pocketmagic.android.openmxplayer;
 
 
 public interface PlayerEvents {
-	public void onStart(String mime, int sampleRate,int channels, long duration);
-	public void onPlay();
-	public void onPlayUpdate(int percent, long currentms, long totalms);
-	public void onStop();
-	public void onError();
+	void onStart(String mime, int sampleRate, int channels, long duration);
+	void onPlay();
+	void onPlayUpdate(int percent, long currentms, long totalms);
+	void onStop();
+	void onError();
 }
  
